566ba7570c9cdb7007bc5b4096414350f04a7ac3
XcelevatorSrimathi <srimathi.2535@xcelevate.org>
2025-07-30 11:59:24 +0530

    FCP005

---
+++ FCP005.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+public class FCP005{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+int quotient=a/b;
+int remainder=a%b;
+System.out.println(quotient);
+System.out.println(remainder);
+}
+}
