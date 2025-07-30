c0505ab8709c941e0c07eaa62cef02f09d3d3f4f
XcelevatorSrimathi <srimathi.2535@xcelevate.org>
2025-07-30 11:56:43 +0530

    FCP001

---
+++ FCP001.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+public class FCP001{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int date=sc.nextInt();
+sc.nextLine();
+String month=sc.nextLine();
+int year=sc.nextInt();
+System.out.println(date);
+System.out.println(month);
+System.out.println(year);
+
+}}
+
+
+
