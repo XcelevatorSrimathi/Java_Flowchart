1b375f070a19af385c74dee0d37380d912d08fbe
XcelevatorSrimathi <srimathi.2535@xcelevate.org>
2025-07-30 12:01:03 +0530

    FCP009

---
+++ FCP009.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class FCP009{
+public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt(); 
+int result=(a-1)-(a- 1) % b;
+System.out.println(result);
+}
+}
