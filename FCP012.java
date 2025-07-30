60c6a1c9d4e88d35f3e134710282a8838a92f9de
XcelevatorSrimathi <srimathi.2535@xcelevate.org>
2025-07-30 12:02:00 +0530

    FCP012

---
+++ FCP012.java
@@ -0,0 +1,20 @@
+
+
+import java.util.*;
+
+public class FCP012{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int a =sc.nextInt();
+if(a==0){
+System.out.println("ZERO");
+}
+else if(a>0)
+{
+System.out.println("POSITIVE");
+}
+else
+{
+System.out.println("NEGATIVE");
+}}}
+
