2b914912dc7c6633b3fb74a8289bdbca6f4f3b22
XcelevatorSrimathi <srimathi.2535@xcelevate.org>
2025-08-02 16:16:23 +0530

    FCP026

---
+++ FCP026.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class FCP026{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+long M=sc.nextLong();
+long N=sc.nextLong();
+long sum=0;
+
+
+for (long i=M;i<=N;i++){
+sum=sum+i;
+}
+System.out.println(sum);
+}}
+
+
+
+
+
+
