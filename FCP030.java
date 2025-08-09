c1ec9cf7808cc9c246b24e1dc20eab55d5459f5c
XcelevatorSrimathi <srimathi.2535@xcelevate.org>
2025-08-09 15:44:50 +0530

    FCP030

---
+++ FCP030.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class FCP030{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int N=sc.nextInt();
+int i=0;
+for(i=0;i*i<N;i++){
+}
+if(i*i==N){
+System.out.println("Yes");
+}
+else{
+System.out.println("No");
+}}}
+
+
+
+
