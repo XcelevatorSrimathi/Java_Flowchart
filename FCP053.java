55c69134620fed6c168788877ac74966151b7d84
XcelevatorSrimathi <srimathi.2535@xcelevate.org>
2025-10-02 11:41:36 +0530

    Add files via upload

---
+++ FCP053.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class FCP053{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int N=sc.nextInt();
+int[]arr=new int[N];
+for (int i=0;i<N;i++){
+arr[i]=i+1;
+}
+for(int i=0;i<N;i++){
+System.out.println(arr[i]);
+}
+}}
+
+
